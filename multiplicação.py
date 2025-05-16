f63976879e9bee290201edf671bed1a6b3ce3dbb
nascimento3322 <vn62511@gmail.com>
2025-05-16 15:27:13 -0300

    Multiplicando

---
+++ multiplicação.py
@@ -0,0 +1,6 @@
+#Multiplicação
+e = int(input("Digite um número: "))
+f = int(input("Digite mais um valor para fazer a multiplicação: "))
+multiplicação = e * f
+print(f"O resultado final da subtração é: {multiplicação}")
+
